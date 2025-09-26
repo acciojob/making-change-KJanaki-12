c7bf4f8890f81aaa849064aed619fb4a41c986b7
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-09-26 19:14:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const makeChange = (c) => {
+
+
+const makeChange = (amount) => {
   // your name here
+	const change = {};
+
+  change.q = Math.floor(amount / 25);     // number of quarters
+  amount %= 25;                           // remainder after quarters
+
+  change.d = Math.floor(amount / 10);     // number of dimes
+  amount %= 10;
+
+  change.n = Math.floor(amount / 5);      // number of nickels
+  amount %= 5;
+
+  change.p = amount;                      // remaining pennies
+
+  return change;
+	
 };
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
+
 alert(JSON.stringify(makeChange(c)));
